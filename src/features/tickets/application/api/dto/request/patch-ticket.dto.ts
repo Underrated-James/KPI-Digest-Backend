8d4e0ee239f83d1545b625b1cd8e5d6a79d0005f
NB000008\Dexter James Quinay <dexter.quinay@opsolutions.biz>
2026-03-31 11:50:38 +0800

    KPI-Digest | 28/03/2026 | All Mappers for All Features

--- src/features/tickets/application/api/dto/request/patch-ticket.dto.ts
+++ src/features/tickets/application/api/dto/request/patch-ticket.dto.ts
@@ -1,10 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTicketDto } from './create-ticket-dto';
-import { IsEnum, IsOptional } from 'class-validator';
-import { TicketStatus } from '../../../../domain/enums/ticket-status';
 
-export class PatchTicketDto extends PartialType(CreateTicketDto) {
-    @IsOptional()
-    @IsEnum(TicketStatus)
-    status?: TicketStatus;
-}
+export class PatchTicketDto extends PartialType(CreateTicketDto) {}
